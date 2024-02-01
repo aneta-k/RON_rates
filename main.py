3366a0b5c57b77434c1fc235301f13c7b4f3875f
aneta-k <kusnierz.aneta@gmail.com>
2024-02-01 14:47:37 +0100

    Initialization of main functions

---
+++ main.py
@@ -0,0 +1,18 @@
+import requests
+import sqlite3
+from datetime import datetime
+
+def get_latest_ron_exchange_rate():
+    pass
+
+def get_historical_ron_exchange_rate():
+    pass
+
+def save_exchange_rate_to_db():
+    pass
+
+def menu():
+    pass
+
+if __name__ == "__main__":
+    menu()
