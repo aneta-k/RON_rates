a066aaeae8f9b81d6f4e4d483a816e8ed9bccd19
aneta-k <kusnierz.aneta@gmail.com>
2024-02-01 15:46:27 +0100

    Return valid data from function and add current date

--- main.py
+++ main.py
@@ -14,7 +14,19 @@ def get_latest_ron_exchange_rate():
     response = requests.get(url)
     data = response.json()
 
-    print(data)
+    if data.get('result') == 'success':
+        conversion_rates = data.get('conversion_rates', {})
+        current_date = datetime.now().strftime('%Y-%m-%d')
+
+        result_data = {
+            'date': current_date,
+            'conversion_rates': conversion_rates
+        }
+
+        return result_data
+    else:
+        print(f"Error: {data.get('error', {}).get('info')}")
+        return None
 
 def get_historical_ron_exchange_rate():
     pass
@@ -23,7 +35,16 @@ def save_exchange_rate_to_db():
     pass
 
 def menu():
-    get_latest_ron_exchange_rate()
+    result_data = get_latest_ron_exchange_rate()
+
+    if result_data:
+        date = result_data.get('date')
+        conversion_rates = result_data.get('conversion_rates')
+
+        print(f"Date: {date}")
+        print("Conversion Rates:")
+        for currency_code, rate in conversion_rates.items():
+            print(f"{currency_code}: {rate}")
 
 if __name__ == "__main__":
     menu()
